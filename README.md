2b897ae2260e8f7fabc4da526876060bb06cb941
mattkrco <matt@immersified.com>
2023-04-30 10:55:16 -0500

    setting up reproducible example

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cdk-issue
+
+This is a simplified sample stack involving one dynamo table and one lambda function. In the real world, this stack involves dozens of tables and dozens of lambda functions.
+
+The goal is to separate the stack into two separate stacks - one for dynamo and one for lambda.
+
+Each table and lambda is defined at the beginning of cdk_app_stack, and then dynamically created by CDK. Each lambda function should only be given access to those tables in its definition.
--- cdk-app/cdk_app/cdk_app_stack.py
+++ cdk-app/cdk_app/cdk_app_stack.py
@@ -8,97 +8,57 @@ from aws_cdk import aws_events as events
 from aws_cdk import aws_iam as iam
 from constructs import Construct
 
+lambda_defs = [
+    {
+        'id': 'sampleLambda',
+        'tables_used': [
+            'sampleTable'
+        ]
+    }
+]
+
+table_defs = [
+    {
+        'id': 'sampleTable',
+        'partition_key': "PartitionKey"
+    }
+]
+
 
 class CdkAppStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        response_lambda = _lambda.Function(self,
-            id="sampleLambda",
-            runtime=_lambda.Runtime.PYTHON_3_9,
-            code=_lambda.Code.from_asset("lambda"),
-            handler="lambda.handler")
+        table_objects = {}
 
-        auth_lambda = _lambda.Function(self,
-            id="authLambda",
-            runtime=_lambda.Runtime.PYTHON_3_9,
-            code=_lambda.Code.from_asset("lambda"),
-            handler="authorizer.handler")
+        for table_def in table_defs:
+            sample_table = _dynamodb.Table(self,
+                id=table_def['id'],
+                partition_key=_dynamodb.Attribute(
+                    name=table_def['partition_key'],
+                    type=_dynamodb.AttributeType.STRING
+                ),
+                billing_mode=_dynamodb.BillingMode.PAY_PER_REQUEST,
+                removal_policy=aws_cdk.RemovalPolicy.DESTROY
+            )
 
-        api = _apigw.RestApi(
-            self,
-            "sampleApi",
-            default_cors_preflight_options={
-                'allow_origins': _apigw.Cors.ALL_ORIGINS,
-                'allow_methods': ["POST", "OPTIONS"],
-                'allow_headers': [
-                    'Content-Type',
-                    'X-Amz-Date',
-                    'Authorization',
-                    'X-Api-Key',
-                    'X-Amz-Security-Token'
-                ]
-            }
-        )
+            table_objects[table_def['id']] = sample_table
 
-        auth = _apigw.CfnAuthorizer(self, "sampleAuthorizer",
-                                                name="sampleAuthorizer",
-                                                rest_api_id=api.rest_api_id,
-                                                type="REQUEST",
+        for lambda_def in lambda_defs:
+            lambda_env_data={}
 
-                                                authorizer_result_ttl_in_seconds=1,
-                                                authorizer_uri=f"arn:aws:apigateway:{aws_cdk.Aws.REGION}:lambda:path/2015-03-31/functions/{auth_lambda.function_arn}/invocations",
-                                                identity_source=_apigw.IdentitySource.header('auth-header'))
+            for table_used in lambda_def['tables_used']:
+                lambda_env_data[f'TABLENAME{table_used.upper()}'] = table_objects[table_used].table_name
 
-        authorizer_id = auth.attr_authorizer_id
+            sample_lambda = _lambda.Function(self,
+                id=lambda_def['id'],
+                runtime=_lambda.Runtime.PYTHON_3_9,
+                code=_lambda.Code.from_asset("lambda"),
+                handler="lambda.handler",
+                environment=lambda_env_data
+            )
 
-        auth_lambda.grant_invoke(iam.ServicePrincipal("apigateway.amazonaws.com"))
+            for table_used in lambda_def['tables_used']:
+                table_objects[table_used].grant_read_write_data(sample_lambda)
 
-        auth_ifunction = _lambda.Function.from_function_arn(self,
-                                                            "authIFunction",
-                                                            auth_lambda.function_arn
-                                                            )
-
-        auth2 = _apigw.RequestAuthorizer(self, 
-                                        "sampleAuthorizer2",
-                                        identity_sources=[_apigw.IdentitySource.header('auth-header')],
-                                        handler=auth_ifunction,
-                                        results_cache_ttl=aws_cdk.Duration.seconds(1)
-                                        )
-        
-        res = api.root.add_resource("sample-resource")
-
-        lambda_integration = _apigw.LambdaIntegration(
-            response_lambda,
-            proxy=False,
-            integration_responses=[
-                {
-                    "statusCode": "200",
-                    "responseParameters": {
-                        "method.response.header.Access-Control-Allow-Origin": "'*'",
-                    },
-                }
-            ],
-        )
-
-        method = res.add_method(
-            "POST",
-            lambda_integration,
-            #authorization_type=_apigw.AuthorizationType.CUSTOM,
-            #authorizer=auth,
-            authorizer=auth2,
-            method_responses=[
-                {
-                    "statusCode": "200",
-                    "responseParameters": {
-                        "method.response.header.Access-Control-Allow-Origin": True,
-                    },
-                }
-            ]
-        )
-
-        # method_resource = method.node.find_child('Resource')
-        # method_resource.add_property_override('AuthorizationType', 'CUSTOM')
-        # method_resource.add_property_override('AuthorizerId', {
-        #     "Ref": authorizer_id})
